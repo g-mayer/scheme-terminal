eaaf80cf965124535ab253a4ff2ff9d45a953137
g-mayer <g-mayer@berkeley.edu>
2020-04-20 17:10:40 -0700

    Finished scheme

--- questions.scm
+++ questions.scm
@@ -8,7 +8,10 @@
 ;; Returns a list of two-element lists
 (define (enumerate s)
   ; BEGIN PROBLEM 15
-  'replace-this-line
+  (define (indexer i lst)
+      (if (null? lst) nil
+        (cons (cons i (cons (car lst) nil)) (indexer (+ i 1) (cdr lst)))))
+    (indexer 0 s)
   )
   ; END PROBLEM 15
 
@@ -18,8 +21,13 @@
 ;; the merged lists.
 (define (merge comp list1 list2)
   ; BEGIN PROBLEM 16
-  'replace-this-line
+  (cond
+      ((null? list1) list2)
+      ((null? list2) list1)
+      ((comp (car list1) (car list2)) (append (list (car list1)) (merge comp (cdr list1) list2)))
+      (else (append (list (car list2)) (merge comp list1 (cdr list2))))
   )
+)
   ; END PROBLEM 16
 
 
@@ -32,8 +40,14 @@
 
 (define (nondecreaselist s)
     ; BEGIN PROBLEM 17
-    'replace-this-line
-    )
+    (cond 
+          ((null? s) nil)
+          ((null? (cdr s)) (list s))
+          ((> (car s) (cadr s))
+              (cons (list (car s)) (nondecreaselist (cdr s))))
+          (else (let ((next (nondecreaselist (cdr s))))
+                     (cons (cons (car s) (car next)) (cdr next))))
+          ))
     ; END PROBLEM 17
 
 
